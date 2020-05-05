5a267dcb0cc8aa35bae754b75c2d15065f3cf499
trevorstam <trevorstam@gmail.com>
2020-05-04 17:25:33 -0700

    timestamp to date

--- postgres_etl_project/sql_queries.py
+++ postgres_etl_project/sql_queries.py
@@ -8,7 +8,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(songplay_id int PRIMARY KEY, start_time timestamp NOT NULL, user_id int NOT NULL, level int, song_id varchar, artist_id int, session_id int, location varchar, user_agent varchar)
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(songplay_id int PRIMARY KEY, start_time date NOT NULL, user_id int NOT NULL, level int, song_id varchar, artist_id int, session_id int, location varchar, user_agent varchar)
 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users(user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar NOT NULL)
@@ -20,7 +20,7 @@ song_table_create = ("""CREATE TABLE IF NOT EXISTS songs(song_id varchar PRIMARY
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists(artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude numeric, longitude numeric)
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time timestamp PRIMARY KEY, hour int, day int, week int, month int, year int, weekday int)
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time date PRIMARY KEY, hour int, day int, week int, month int, year int, weekday int)
 """)
 
 # INSERT RECORDS
@@ -56,5 +56,5 @@ AND s.duration = %s
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
